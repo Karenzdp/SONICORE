a7a8a1aab150ad86986621487501d3cd4399127d
Karenzdp <kdzapata22@ucatolica.edu.co>
2025-11-03 17:30:57 -0500

    Corrige imports y mejora organización del main.py

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ from routes.generos import router as generos
 from routes.albumes import router as albumes
 from routes.canciones import router as canciones
 from routes.api import router as apis
+from routes.usuario import router as usuarios
+
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+    
 
-app.include_router(artistas)
-app.include_router(generos)
-app.include_router(albumes)
-app.include_router(canciones)
-app.include_router(apis)
+app.include_router(usuarios, prefix="/usuarios", tags=["Usuarios"])
+app.include_router(artistas, prefix="/artistas", tags=["Artistas"])
+app.include_router(generos, prefix="/generos", tags=["Géneros"])
+app.include_router(albumes, prefix="/albumes", tags=["Álbumes"])
+app.include_router(canciones, prefix="/canciones", tags=["Canciones"])
+app.include_router(apis, prefix="/apis", tags=["APIs Externas"])
+
 @app.get("/")
 def read_root():
     return {"mensaje": "Bienvenido a SONICORE - API DE MÚSICA"}
